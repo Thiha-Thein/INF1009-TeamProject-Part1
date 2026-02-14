5991f499a8c8be6329bd079fadb463bafb217f78
Kevin Thiha <kevinyan200307@gmail.com>
2026-02-14 21:39:04 +0800

    Added IOManager and SoundManager implementation. Edited GameMaster and MainObject to integrate IOManger and SoundManger

--- core/src/main/java/io/github/some_example_name/AbstractEngine/GameMaster.java
+++ core/src/main/java/io/github/some_example_name/AbstractEngine/GameMaster.java
@@ -2,13 +2,18 @@ package io.github.some_example_name.AbstractEngine;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 
+import io.github.some_example_name.AbstractEngine.Audio.SoundManager;
 import io.github.some_example_name.AbstractEngine.EntityManagement.EntityManager;
+import io.github.some_example_name.AbstractEngine.IO.IOManager;
 
 public class GameMaster extends ApplicationAdapter {
 
-    // Add EntityManager
     private EntityManager entityManager;
+    private IOManager ioManager;
+    private SoundManager soundManager;
+
     private boolean isInitialized = false;
 
     @Override
@@ -22,34 +27,50 @@ public class GameMaster extends ApplicationAdapter {
         update(deltaTime);
     }
 
-    // Unity-style Start method - called once at the beginning
     public void start() {
-        if (isInitialized) {
-            return;
-        }
+        if (isInitialized) return;
 
         System.out.println("GameMaster: Starting game...");
 
-        // Initialize EntityManager
+        // Managers
         entityManager = new EntityManager();
-        MainObject player = new MainObject();
+        ioManager = new IOManager();
+        soundManager = new SoundManager();
+        
+        // Load audio
+        soundManager.loadSound("jumpSfx", "sfx/jump.wav");
+        soundManager.loadMusic("bgm", "music/bgm.mp3");
+
+        // Start background music
+        soundManager.playMusic("bgm", true);
+
+        ioManager.bindKey("up", Input.Keys.W);
+        ioManager.bindKey("down", Input.Keys.S);
+        ioManager.bindKey("left", Input.Keys.A);
+        ioManager.bindKey("right", Input.Keys.D);
+        ioManager.bindKey("jump", Input.Keys.SPACE);
+
+        
+        MainObject player = new MainObject(ioManager, soundManager);
         entityManager.addEntity(player);
 
         isInitialized = true;
         entityManager.init();
+
         System.out.println("GameMaster: Game started!");
     }
 
     // Unity-style Update method - called every frame
     public void update(float deltaTime) {
-        if (!isInitialized) {
-            return;
+        if (!isInitialized) return;
+
+        ioManager.update();
+
+        if (ioManager.wasPressed("jump")) { 
+        	soundManager.playSound("jumpSfx");
         }
 
-        // Update all entities
         entityManager.updateAll(deltaTime);
-
-        // Render all entities
         entityManager.renderAll();
     }
 
@@ -57,16 +78,14 @@ public class GameMaster extends ApplicationAdapter {
     public void dispose() {
         System.out.println("GameMaster: Cleaning up...");
 
-        if (entityManager != null) {
-            entityManager.clear();
-        }
+        if (entityManager != null) entityManager.clear();
+        if (soundManager != null) soundManager.dispose();
 
         System.out.println("GameMaster: Cleanup complete!");
     }
 
-    // Getter for EntityManager (so other classes can access it)
-    public EntityManager getEntityManager() {
-        return entityManager;
-    }
+    // Getters
+    public EntityManager getEntityManager() { return entityManager; }
+    public IOManager getIOManager() { return ioManager; }
+    public SoundManager getSoundManager() { return soundManager; }
 }
-
--- core/src/main/java/io/github/some_example_name/AbstractEngine/MainObject.java
+++ core/src/main/java/io/github/some_example_name/AbstractEngine/MainObject.java
@@ -1,26 +1,51 @@
 package io.github.some_example_name.AbstractEngine;
+
+import io.github.some_example_name.AbstractEngine.Audio.SoundManager;
 import io.github.some_example_name.AbstractEngine.EntityManagement.Entity;
 import io.github.some_example_name.AbstractEngine.EntityManagement.Transform;
+import io.github.some_example_name.AbstractEngine.IO.IOManager;
 
-public class MainObject extends Entity{
+public class MainObject extends Entity {
 
+    private final IOManager io;
+    private final SoundManager sound;
+
+    private float moveSpeed = 200f; 
+
+    public MainObject(IOManager io, SoundManager sound) {
+        this.io = io;
+        this.sound = sound;
+    }
 
     @Override
     public void start() {
         setTag("Player");
         setActive(true);
-        Transform transform1 = new Transform(100,100,32,32);
+
+        transform = new Transform(100, 100, 32, 32);
+
     }
 
     @Override
     public void update(float deltaTime) {
-        transform.translate(1,-2);
-        System.out.println("MainObject: " + transform.getX() + transform.getY());
+        float dx = 0;
+        float dy = 0;
+
+        if (io.isDown("right")) dx += moveSpeed * deltaTime;
+        if (io.isDown("left"))  dx -= moveSpeed * deltaTime;
+        if (io.isDown("up"))    dy += moveSpeed * deltaTime;
+        if (io.isDown("down"))  dy -= moveSpeed * deltaTime;
+
+        transform.translate(dx, dy);
+
+        if (io.wasPressed("jump")) {
+            System.out.println("Jump pressed!");
+        }
+
+        System.out.println("MainObject Position: (" + transform.getX() + ", " + transform.getY() + ")");
     }
 
     @Override
     public void render() {
-
     }
 }
-
